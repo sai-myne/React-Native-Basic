d5e64f061368cb8d2d59412d1c9afbb875552d9f
sai-myne <ahlusir@gmail.com>
2020-07-29 14:31:44 +0800

    Todo App part 3

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList  } from 'react-native';
 import Header from './components/header'
 import TodoItem from './components/todoItem'
+import AddTodo from './components/AddTodo'
 
 export default function App(){
   const [todos, setTodos] = useState([
@@ -16,12 +17,22 @@ export default function App(){
     })
   }
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        { text: text, key: Math.random().toString() },
+        ...prevTodos
+      ];
+    })
+  }
+
   return (
     <View style={styles.container}>
       {/* header */}
       <Header />
       <View style={styles.content}>
         {/* to form */}
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList 
             data = {todos}
