3b38e62fb6b64e10d19cb99751a662f8f8211730
sai-myne <ahlusir@gmail.com>
2020-07-29 14:40:25 +0800

    Dismming the keyboard

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Alert  } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert,TouchableWithoutFeedback, Keyboard  } from 'react-native';
 import Header from './components/header'
 import TodoItem from './components/todoItem'
 import AddTodo from './components/AddTodo'
@@ -35,23 +35,29 @@ export default function App(){
   }
 
   return (
-    <View style={styles.container}>
-      {/* header */}
-      <Header />
-      <View style={styles.content}>
-        {/* to form */}
-        <AddTodo submitHandler={submitHandler} />
-        <View style={styles.list}>
-          <FlatList 
-            data = {todos}
-            renderItem = {({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler} />
-            )}
-          />
-        </View>
-      </View>      
-      
-    </View>
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismissed keyboaard');
+    }}>
+      <View style={styles.container}>
+        {/* header */}
+        <Header />
+        <View style={styles.content}>
+          {/* to form */}
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList 
+              data = {todos}
+              renderItem = {({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>      
+        
+      </View>
+    </TouchableWithoutFeedback>
+    
   );
 }
 
