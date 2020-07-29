b9d8a1fdc09305eaadcc0394907d354400e7cc28
sai-myne <ahlusir@gmail.com>
2020-07-29 16:08:13 +0800

    Global Styles

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Home from './screens/home';
-import { useFonts } from 'expo-font';
+import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 
-
-
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+});
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
-    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
-  });
-  // const [fontsLoaded, setFontsLoaded] =  useState(false);
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  if(fontsLoaded){
+  if (fontsLoaded) {
     return (
       <Home />
     );
   } else {
     return (
-      <AppLoading />
+      <AppLoading 
+        startAsync={getFonts} 
+        onFinish={() => setFontsLoaded(true)} 
+      />
     )
   }
+
 }
-  
 
 
