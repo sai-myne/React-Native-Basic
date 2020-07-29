806a998e9c1affa0ccf375e2b527f0ac88f9c279
sai-myne <ahlusir@gmail.com>
2020-07-29 16:01:12 +0800

    Custom Fonts

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Home from './screens/home';
+import { useFonts } from 'expo-font';
+import { AppLoading } from 'expo';
+
+
+
 
 export default function App() {
-  return (
-    <Home />
-  );
+  let [fontsLoaded] = useFonts({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
+  // const [fontsLoaded, setFontsLoaded] =  useState(false);
+
+  if(fontsLoaded){
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading />
+    )
+  }
 }
+  
 
 
