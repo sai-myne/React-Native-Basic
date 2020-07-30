924cdf18580f0aaa4d75b725c5524c5a81886f87
sai-myne <ahlusir@gmail.com>
2020-07-30 13:13:14 +0800

    Showing Forms Errors

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -1,56 +1,68 @@
-import React from 'react';
-import { StyleSheet, Button, TextInput, View, Text } from 'react-native';
-import { globalStyles } from '../styles/global';
-import { Formik } from 'formik';
-import * as yup from 'yup';
+import React from "react";
+import { StyleSheet, Button, TextInput, View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
+import { Formik } from "formik";
+import * as yup from "yup";
 
 const reviewSchema = yup.object({
-    title: yup.string().required().min(4),
-    body: yup.string().required().min(8),
-    rating: yup.string().required().test('is-num-1-5', 'Rating must be a number 1 - 5', (val) => {
-        return parseInt(val) < 6 && parseInt(val) > 0;
-    })
-})
+  title: yup.string().required().min(4),
+  body: yup.string().required().min(8),
+  rating: yup
+    .string()
+    .required()
+    .test("is-num-1-5", "Rating must be a number 1 - 5", (val) => {
+      return parseInt(val) < 6 && parseInt(val) > 0;
+    }),
+});
 
-export default function ReviewForm({ addReview }){
+export default function ReviewForm({ addReview }) {
+  return (
+    <View style={globalStyles.container}>
+      <Formik
+        initialValues={{ title: "", body: "", rating: "" }}
+        validationSchema={reviewSchema}
+        onSubmit={(values, actions) => {
+          actions.resetForm();
+          addReview(values);
+        }}
+      >
+        {(props) => (
+          <View>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Review title"
+              onChangeText={props.handleChange("title")}
+              value={props.values.title}
+              onBlur={props.handleBlur('title')}
+            />
+            <Text style={globalStyles.errorText}>{ props.touched.title && props.errors.title }</Text>
+            <TextInput
+              multiline
+              style={globalStyles.input}
+              placeholder="Review body"
+              onChangeText={props.handleChange("body")}
+              value={props.values.body}
+              onBlur={props.handleBlur('body')}
+            />
+            <Text style={globalStyles.errorText}>{ props.touched.body && props.errors.body }</Text>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Rating (1-5)"
+              onChangeText={props.handleChange("rating")}
+              value={props.values.rating}
+              keyboardType="numeric"
+              onBlur={props.handleBlur('rating')}
+            />
+            <Text style={globalStyles.errorText}>{ props.touched.rating && props.errors.rating }</Text>
 
-    return(
-        <View style={globalStyles.container}>
-            <Formik
-                initialValues={{ title: '', body: '', rating: '' }}
-                validationSchema={reviewSchema}
-                onSubmit={(values, actions) => {
-                    actions.resetForm();
-                    addReview(values);
-                }}
-            >
-                {(props) => (
-                    <View>
-                        <TextInput
-                            style={globalStyles.input}
-                            placeholder= 'Review title'
-                            onChangeText={props.handleChange('title')}
-                            value={props.values.title}
-                        />
-                        <TextInput
-                            multiline
-                            style={globalStyles.input}
-                            placeholder= 'Review body'
-                            onChangeText={props.handleChange('body')}
-                            value={props.values.body}
-                        />
-                        <TextInput
-                            style={globalStyles.input}
-                            placeholder= 'Rating (1-5)'
-                            onChangeText={props.handleChange('rating')}
-                            value={props.values.rating}
-                            keyboardType='numeric'
-                        />
-
-                        <Button title='submit' color='maroon' onPress={props.handleSubmit} />
-                    </View>
-                )}
-            </Formik>
-        </View>
-    )
+            <Button
+              title="submit"
+              color="maroon"
+              onPress={props.handleSubmit}
+            />
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
 }
