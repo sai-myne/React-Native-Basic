9ac099475217cf5e0ee99e5604a5706d75aa39fd
sai-myne <ahlusir@gmail.com>
2020-07-30 09:47:35 +0800

    Navigation Options

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -6,13 +6,26 @@ import ReviewDetails from '../screens/reviewDetails';
 const screens = {
   Home: {
     screen: Home,
+    navigationOptions: {
+      title: 'GameZone',
+      // headerStyle: { backgroundColor: '#eee' }
+    }
   },
   ReviewDetails: {
     screen: ReviewDetails,
+    navigationOptions: {
+      title: 'Review Details',
+      // headerStyle: { backgroundColor: '#eee' }
+    }
   },
 };
 
 // home stack navigator screens
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: '#444',
+    headerStyle: { backgroundColor: '#eee', height: 80 }
+  }
+});
 
 export default createAppContainer(HomeStack);
